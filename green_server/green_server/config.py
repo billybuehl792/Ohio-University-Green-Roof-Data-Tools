ca3a3c87d9686c819dc0bb1d7314eb25e0b2b95b
Billy Buehl <billybuehl792@gmail.com>
2020-11-23 17:55:23 -0500

    config file changes

--- green_server/green_server/config.py
+++ green_server/green_server/config.py
@@ -1,8 +1,15 @@
 
+import os
+import json
+
+config_file = '/etc/config/configuration.json'
+with open(config_file, 'r') as f:
+    config = json.load(f)
+
 class Config:
 
-    SECRET_KEY = 'fabc4a74745faa12344628eabba7787a'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site_1.db'
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
     CSRF_ENABLED = True
